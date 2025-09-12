f8730ff0ab5b38e27584815689f34b7f0f2d3b34
giabao-24 <baocaohuy123@gmail.com>
2025-09-12 10:27:11 +0700

    nothing change,just test GIT

--- style2.js
+++ style2.js
@@ -1,9 +1,11 @@
 function closeleftnavbar() {
    document.getElementById("leftnavbar").style.display = "none";
+   document.getElementById("leftnavbarbutton").style.opacity = "flex";
 }
 function rename() {
   document.getElementById("getnameuser").style.display = "flex";
 }
 function openleftnavbar() {
   document.getElementById("leftnavbar").style.display = "flex";
+    document.getElementById("leftnavbarbutton").style.opacity = "none";
 }
