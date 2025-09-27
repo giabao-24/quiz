4f68490662cbb3feedd9d613beb916fe454169fa
giabao-24 <baocaohuy123@gmail.com>
2025-09-27 16:07:39 +0700

    add page2-footer...

--- style2.js
+++ style2.js
@@ -2,10 +2,16 @@ function closeleftnavbar() {
    document.getElementById("leftnavbar").style.display = "none";
    document.getElementById("leftnavbarbutton").style.opacity = "flex";
 }
+
 function rename() {
   document.getElementById("getnameuser").style.display = "flex";
 }
+
 function openleftnavbar() {
   document.getElementById("leftnavbar").style.display = "flex";
     document.getElementById("leftnavbarbutton").style.opacity = "none";
+}
+
+function nextexam() {
+  window.location.href = "page2.html";
 }
